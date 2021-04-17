80907631955555722209d75da8c42f92628101da
Siddharth Agrawal <agrawalsiddharth329@gmail.com>
2021-04-17 14:42:05 +0530

    React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<React.Fragment className="App">
+			<h1>Welcome To ProShop</h1>
+		</React.Fragment>
+	);
+};
+
+export default App;
